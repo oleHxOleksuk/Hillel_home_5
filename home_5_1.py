4c5f0704ba69bedd4c26fc2f4a71f142aad6f2de
oleHxOleksuk <108345191+oleHxOleksuk@users.noreply.github.com>
2025-04-03 19:59:52 +0300

    5_1

--- home_5_1.py
+++ home_5_1.py
@@ -1,14 +1,28 @@
 import keyword
 import string
-from operator import index
 
-name = input('Введіть імя змінної: ')
+name = input("Введіть ім'я змінної: ")
 
+    # Перевірка на наявність зареєстрованого слова
 if name in keyword.kwlist:
     print(False)
+
+    # Перевірка на те, чи починається з цифри
 elif name[0].isdigit():
     print(False)
-elif string.punctuation in index(name):
-    print('klhklh')
+
+    # Перевірка на наявність великих літер
+elif any(char.isupper() for char in name):
+    print(False)
+
+    # Перевірка на наявність заборонених символів
+elif any(char in string.punctuation and char != "_" for char in name):
+    print(False)
+
+    # Перевірка на кількість підкреслень
+elif name.count('_') > 1:
+    print(False)
 else:
     print(True)
+
+
